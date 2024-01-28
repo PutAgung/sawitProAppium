9049fb17fabd82434b570104be6efa36c7332c2d
Putra Agung Pratama <uthaagung@gmail.com>
2024-01-28 11:16:22 +0700

    new commit

---
+++ src/test/java/AppiumTest/LoginScenarioTest.java
@@ -0,0 +1,79 @@
+package AppiumTest;
+
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import junit.framework.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class LoginScenarioTest {
+
+    private AppiumDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options
+                .setIgnoreHiddenApiPolicyError(true)
+                .setFullReset(true)
+                .setApp("C:\\Users\\ASUS R565\\IdeaProjects\\APKFiles\\Chat21.apk")
+                .setPlatformName("Android")
+                .setDeviceName("bffa85a2")
+                .setAutomationName("uiautomator2")
+                .setAppPackage("chat21.android.demo")
+                .setAppActivity("chat21.android.demo.SplashActivity")
+                .setAutoGrantPermissions(true);
+
+        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), options);
+        System.out.println("--- Successfully Go inside the apps ---");
+        driver.findElement(By.id("android:id/button1")).click();
+
+    }
+
+    @Test
+    public void testLoginLogout() {
+        driver.findElement(By.id("chat21.android.demo:id/email")).sendKeys("putrakatalon@gmail.com");
+        driver.findElement(By.id("chat21.android.demo:id/password")).sendKeys("Kucing123!");
+        driver.findElement(By.id("chat21.android.demo:id/login")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("chat21.android.demo:id/toolbar")));
+        Assert.assertTrue(driver.findElement(By.id("chat21.android.demo:id/toolbar")).isDisplayed());
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='PROFILE']")));
+        driver.findElement(By.xpath("//android.widget.TextView[@text='PROFILE']")).click();
+        driver.findElement(By.id("chat21.android.demo:id/logout")).click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("chat21.android.demo:id/login")));
+        Assert.assertTrue(driver.findElement(By.id("chat21.android.demo:id/login")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.id("chat21.android.demo:id/signup")).isDisplayed());
+    }
+
+    @Test
+    public void testWrongEmail(){
+        driver.findElement(By.id("chat21.android.demo:id/email")).sendKeys("email");
+        driver.findElement(By.id("chat21.android.demo:id/password")).sendKeys("Kucing123!");
+        driver.findElement(By.id("chat21.android.demo:id/login")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("chat21.android.demo:id/toolbar")));
+        Assert.assertTrue(driver.findElement(By.id("chat21.android.demo:id/toolbar")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.id("chat21.android.demo:id/signup")).isDisplayed());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        System.out.println("--- Done ---");
+        driver.quit();
+    }
+}
